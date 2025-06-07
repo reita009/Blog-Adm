9306191f17a280c35cc89700c4f0dd65ce1bbe3d
Julio Santana <julio.dev.santana@gmail.com>
2025-06-07 11:44:45 -0300

    Initializing the front-end and controllers for the category form

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (re, res) => {
-  res.send("Rota categoria");
+router.get("/admin/categories/new", (re, res) => {
+  res.render("admin/categories/new");
 });
 
 module.exports = router;
