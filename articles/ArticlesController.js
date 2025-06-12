5ab860ba32ca848dafa1ea9bc99c42ad919d501b
Julio Santana <julio.dev.santana@gmail.com>
2025-06-12 15:04:02 -0300

    finish category module and start article module

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,4 +5,8 @@ router.get("/articles", (re, res) => {
   res.send("Rota Artigo!!!!");
 });
 
+router.get("/admin/articles/new", (req, res) => {
+  res.render("admin/articles/new");
+});
+
 module.exports = router;
--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -16,7 +16,7 @@ router.post("/categories/save", (req, res) => {
       title: title,
       slug: slugify(title),
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     });
   } else {
     res.redirect("/admin/categories/new");
@@ -48,6 +48,41 @@ router.post("/categories/delete", (req, res) => {
   } else {
     res.redirect("/admin/categories");
   }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  let id = req.params.id;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+  } else {
+    Category.findByPk(id)
+      .then((category) => {
+        if (category != undefined) {
+          res.render("admin/categories/edit", { category: category });
+        } else {
+          res.redirect("/admin/categories");
+        }
+      })
+      .catch((erro) => {
+        console.log("aconteceu um error" + erro);
+        res.redirect("/admin/categories");
+      });
+  }
+});
+
+router.post("/categories/update", (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title) },
+    {
+      where: { id: id },
+    }
+  ).then(() => {
+    res.redirect("/admin/categories");
+  });
 });
 
 module.exports = router;
