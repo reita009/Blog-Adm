80e3311d5d48d6ef4f4a243dda3cfc5639de5dd0
Julio Santana <julio.dev.santana@gmail.com>
2025-06-17 12:23:56 -0300

    finish form article

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
 
 router.get("/articles", (re, res) => {
   res.send("Rota Artigo!!!!");
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", {
+      categories: categories,
+    });
+  });
 });
 
 module.exports = router;
