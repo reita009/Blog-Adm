b5464f41d1f1eb42e8d11d9a2204b32b5cdfc262
Julio Santana <julio.dev.santana@gmail.com>
2025-06-18 11:15:30 -0300

    update article and database

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugfy = require("slugify");
 
-router.get("/articles", (re, res) => {
-  res.send("Rota Artigo!!!!");
+router.get("/admin/articles", (re, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", {
+      articles: articles,
+    });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
@@ -12,6 +20,26 @@ router.get("/admin/articles/new", (req, res) => {
       categories: categories,
     });
   });
+});
+
+router.post("/articles/save", (req, res) => {
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugfy(title),
+    body: body,
+    categoryId: category,
+  })
+    .then(() => {
+      console.log("Deu certo!");
+      res.redirect("/admin/articles");
+    })
+    .catch((err) => {
+      console.log("erro:" + err);
+    });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("blogadm", "root", "lira0509", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
